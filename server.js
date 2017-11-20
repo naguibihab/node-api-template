51b8cd28e5eb43f759818712b0297efe625f50f8
Nick Ihab <n.ihab@learnmeter.com>
2017-11-21 10:41:56 +1100

    Added middleware

--- server.js
+++ server.js
@@ -22,3 +22,8 @@ app.listen(port);
 
 
 console.log('todo list RESTful API server started on: ' + port);
+
+// Middleware
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
